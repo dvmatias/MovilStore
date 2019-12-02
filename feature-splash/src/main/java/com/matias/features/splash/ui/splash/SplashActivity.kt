892742b3299cac55aa7c7f9c7278228d1962003a
Matías <matias.delv.dom@gmail.com>
2019-12-02 00:39:51 -0300

    Screen out animation.

--- feature-splash/src/main/java/com/matias/features/splash/ui/splash/SplashActivity.kt
+++ feature-splash/src/main/java/com/matias/features/splash/ui/splash/SplashActivity.kt
@@ -1,6 +1,7 @@
 package com.matias.features.splash.ui.splash
 
 import android.os.Bundle
+import android.os.Handler
 import com.matias.core.base.helpers.startVectorAnimation
 import com.matias.core.base.mvp.BasePresenterActivity
 import com.matias.feature_splash.R
@@ -27,18 +28,19 @@ class SplashActivity :
 	override fun onResume() {
 		super.onResume()
 		animateScreenIn()
+		Handler().postDelayed({animateScreenOut()}, 5000)
 	}
 
 	/*******************************************************************************************************************
 	 * [SplashActivityContract.View] implementation
 	 */
 	override fun animateScreenIn() {
-		startVectorAnimation(imageTopLeftFigure)
-		startVectorAnimation(imageBottomRightFigure)
+		startVectorAnimation(imageTopLeftFigure, getDrawable(R.drawable.vd_splash_top_left_figure_anim_in_3))
+		startVectorAnimation(imageBottomRightFigure, getDrawable(R.drawable.vd_splash_bottom_right_figure_anim_in_3))
 	}
 
 	override fun animateScreenOut() {
-		startVectorAnimation(imageTopLeftFigure, getDrawable(R.drawable.vd_splash_top_left_figure_anim_out_2))
-		startVectorAnimation(imageBottomRightFigure, getDrawable(R.drawable.vd_splash_bottom_right_figure_anim_out_2))
+		startVectorAnimation(imageTopLeftFigure, getDrawable(R.drawable.vd_splash_top_left_figure_anim_out_3))
+		startVectorAnimation(imageBottomRightFigure, getDrawable(R.drawable.vd_splash_bottom_right_figure_anim_out_3))
 	}
 }
