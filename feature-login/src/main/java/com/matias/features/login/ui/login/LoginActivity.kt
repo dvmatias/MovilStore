a2c1947697d592f31fc90fcc86e5c3ceed0755f9
Matías <matias.delv.dom@gmail.com>
2019-12-14 18:57:14 -0300

    Signm In Fragment UI.

--- feature-login/src/main/java/com/matias/features/login/ui/login/LoginActivity.kt
+++ feature-login/src/main/java/com/matias/features/login/ui/login/LoginActivity.kt
@@ -9,35 +9,39 @@ import com.matias.features.login.ui.LoginUiComponent
 import kotlinx.android.synthetic.main.activity_login.*
 import javax.inject.Inject
 
-class LoginActivity : BasePresenterActivity<LoginActivity, LoginActivityPresenter, LoginActivitySubComponent>(),
-        LoginActivityContract.View {
-    
-    @Inject
-    lateinit var pagerAdapter: LoginPagerAdapter
-    
-    private lateinit var tabTitles: List<String>
-    
-    override fun bindComponent(): LoginActivitySubComponent = LoginUiComponent.component.plus(LoginActivityModule(this))
-    
-    override fun bindLayout(): Int = R.layout.activity_login
-    
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        super.applyFullScreen()
-        
-        setupPager()
-        setupTabLayout()
-    }
-    
-    private fun setupPager() {
-        viewPager.adapter = pagerAdapter
-    }
-    
-    private fun setupTabLayout() {
-        tabTitles = listOf(getString(R.string.tab_sign_in_label), getString(R.string.tab_sign_up_label))
-        tabs.setupWithViewPager(viewPager)
-        tabs.setTitlesAtTabs(tabTitles)
-    }
-    
+
+class LoginActivity :
+	BasePresenterActivity<LoginActivity, LoginActivityPresenter, LoginActivitySubComponent>(),
+	LoginActivityContract.View {
+
+	@Inject
+	lateinit var pagerAdapter: LoginPagerAdapter
+
+	private lateinit var tabTitles: List<String>
+
+	override fun bindComponent(): LoginActivitySubComponent =
+		LoginUiComponent.component.plus(LoginActivityModule(this))
+
+	override fun bindLayout(): Int = R.layout.activity_login
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_login)
+		super.applyFullScreen()
+
+		setupPager()
+		setupTabLayout()
+	}
+
+	private fun setupPager() {
+		viewPager.adapter = pagerAdapter
+	}
+
+	private fun setupTabLayout() {
+		tabTitles =
+			listOf(getString(R.string.tab_sign_in_label), getString(R.string.tab_sign_up_label))
+		tabs.setupWithViewPager(viewPager)
+		tabs.setTitlesAtTabs(tabTitles)
+	}
+
 }
